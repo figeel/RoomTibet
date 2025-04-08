b39850da4251c9e3a7aaa8075449da7593894a41
Ilya Lysenko <165323467+figeel@users.noreply.github.com>
2025-04-08 23:20:57 +0400

    Add files via upload

---
+++ js/all-card.js
@@ -0,0 +1,94 @@
+// Данные карточек
+const allCardsData = [
+  {
+    background: "url(img/cards/card-background01.jpeg)",
+    title: "Озеро возле гор",
+    description: "романтическое приключение",
+    price: "480 $",
+    full_description:
+      'Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur"и занялся его поисками в классической латинской литературе.',
+  },
+  {
+    background: "url(img/cards/card-background02.jpeg)",
+    title: "Ночь в горах",
+    description: "в компании друзей",
+    price: "500 $",
+    full_description:
+      'Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur"и занялся его поисками в классической латинской литературе.',
+  },
+  {
+    background: "url(img/cards/card-background03.jpeg)",
+    title: "Йога в горах",
+    description: "для тех, кто забоится о себе",
+    price: "230 $",
+    full_description:
+      'Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur"и занялся его поисками в классической латинской литературе.',
+  },
+  {
+    background: "url(img/cards/card-background04.jpg)",
+    title: "Тур в африку",
+    description: "Веселое сафари",
+    price: "600 $",
+    full_description:
+      'Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur"и занялся его поисками в классической латинской литературе.',
+  },
+  {
+    background: "url(img/cards/card-background05.jpeg)",
+    title: "Амазонка",
+    description: "для тех, кто любит экстрим",
+    price: "100 $",
+    full_description:
+      'Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur"и занялся его поисками в классической латинской литературе.',
+  },
+  {
+    background: "url(img/cards/card-background06.jpeg)",
+    title: "Карелия",
+    description: "Удивительные красоты",
+    price: "150 $",
+    full_description:
+      'Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur"и занялся его поисками в классической латинской литературе.',
+  },
+  // Добавляйте новые объекты для новых карточек
+];
+
+// Функция создания карточки
+function createAllCard(card) {
+    return `
+        <div class="popular-ways-card" style="background-image: ${card.background}">
+            <div class="card-content">
+                <div class="card-content-visible">
+                    <div class="popular-ways-card-text">
+                        <p class="card-title">${card.title}</p>
+                        <p class="card-description">${card.description}</p>
+                    </div>
+                    <div class="card-price">
+                        <p class="card-price-text">${card.price}</p>
+                    </div>
+                </div>
+                <div class="card-full-description">
+                    <p class="card-full-description-text">${card.full_description}</p>
+                </div>
+                <a href="#!" class="button card-button">Программа тура</a>
+            </div>
+        </div>
+    `;
+}
+
+// Функция рендера всех карточек
+function renderCards() {
+    const container = document.querySelector('.all-ways-cards');
+    container.innerHTML = allCardsData.map(card => createAllCard(card)).join('');
+}
+
+// Первоначальный рендер
+renderCards();
+
+// Для добавления новой карточки просто добавьте новый объект в cardsData и вызовите renderCards()
+// Пример:
+// cardsData.push({
+//     background: 'url(../img/new-image.jpg)',
+//     title: 'Новый тур',
+//     description: 'новое описание',
+//     price: '999 $'
+// });
+// renderCards();   
--- js/script.js
+++ js/script.js
@@ -72,17 +72,3 @@ $(function() {
         $('#datepicker').data('daterangepicker').toggle();
     });
 });
-
-// nav button
-
-const nav = document.querySelector('#header-nav');
-const navBtn = document.querySelector('#menu-button');
-const navBtnImg = document.querySelector('#menu-button-img');
-
-navBtn.onclick = () => {
-    if (nav.classList.toggle('open')) {
-        navBtnImg.src = "./img/nav-opened.svg"
-    } else {
-        navBtnImg.src = "./img/nav-closed.svg"
-    }
-}
